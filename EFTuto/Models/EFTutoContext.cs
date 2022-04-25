39f93ff22001e0fc937151fc8cb33eac2e1dcb6b
sachaamm <sachaamm@gmail.com>
2022-04-25 19:54:08 +0200

    v1

---
+++ EFTuto/Models/EFTutoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFTuto.Models
+{
+    public class EFTutoContext : DbContext
+    {
+        public EFTutoContext(DbContextOptions<EFTutoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ EFTuto/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFTuto.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [MaxLength(60)]
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+    }
+}
